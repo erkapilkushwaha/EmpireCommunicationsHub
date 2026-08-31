a4154e3c331e97d7f9135d57d959a7e6c582655a
erkapilkushwaha <kapilkushwaha047@gmail.com>
2026-09-01 00:52:51 +0530

    Remove allowedDevOrigins from next.config.js
    Removed experimental allowedDevOrigins from Next.js config.

--- artifacts/empire-communications-hub/next.config.js
+++ artifacts/empire-communications-hub/next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    allowedDevOrigins: [
-      "*.replit.dev",
-      "*.pike.replit.dev",
-      "693a4034-4aac-4902-ade2-4168ae30e57b-00-wek2nf0qok06.pike.replit.dev",
-      "localhost:3000",
-      "localhost:19304",
-    ],
-  },
 };
 
 module.exports = nextConfig;
+
